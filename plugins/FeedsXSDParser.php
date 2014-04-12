1dbdcaf2c753666f731c77a21cb8e3be6abe18bb
Clemens Tolboom <clemens@build2be.com>
2014-04-12 16:00:04 +0200

    Documentation and code format.

--- plugins/FeedsXSDParser.php
+++ plugins/FeedsXSDParser.php
@@ -1,11 +1,18 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: clemens
- * Date: 02-04-14
- * Time: 15:51
+ * @file
+ * This file contains a XML Parser based on a given XSD.
+ *
+ * The given XSD is processed to get all possible path selectors. Next the user must give a context from which
+ * to extract 'records' for further processing.
  */
 
+/**
+ * Class FeedsXSDParser
+ *
+ * Provides for the different feeds forms and processes the given XML.
+ */
 class FeedsXSDParser extends FeedsXSDParserXML {
 
   /**
@@ -33,6 +40,7 @@ class FeedsXSDParser extends FeedsXSDParserXML {
    * Define default configuration.
    */
   public function configDefaults() {
+    // TODO must these match with sourceDefaults?
     return array(
       // TODO: hardcoded schema
       'xsd_uri' => 'http://schemas.geonovum.nl/stri/2012/1.0/STRI2012.xsd',
@@ -59,18 +67,20 @@ class FeedsXSDParser extends FeedsXSDParserXML {
     $file = file_save_upload('xsd_upload', array(
       'file_validate_extensions' => array('xsd')
     ));
-    if($file){
+    if ($file) {
       $parser = new XsdToObject();
       $xsd = file_get_contents($file->uri);
       $result = $parser->parse($xsd);
       if (count($result) == 0) {
         form_set_error('xsd_upload', t("This doesn't seen to be a valid XSD schema"));
-      }else{
+      }
+      else {
         $values['xsd_upload'] = $file;
         $values['xpaths'] = $result;
       }
 
-    }else{
+    }
+    else {
       form_set_error('xsd_upload', t('No XSD Schema uploaded.'));
     }
 
@@ -87,16 +97,18 @@ class FeedsXSDParser extends FeedsXSDParserXML {
 
   public function getMappingSources() {
     $xpathsources = array();
-    foreach($this->config['xpaths'] as $path=>$properties){
+    foreach ($this->config['xpaths'] as $path => $properties) {
       $xpathsources['xsd:' . $path]['name'] = 'xsd:' . $path;
-      if(isset($properties['annotation'])){
-        if(isset($properties['annotation']['en'])){
+      if (isset($properties['annotation'])) {
+        if (isset($properties['annotation']['en'])) {
           $xpathsources['xsd:' . $path]['description'] = $properties['annotation']['en'];
-        }else{
+        }
+        else {
           $firstlang = array_shift($properties['annotation']);
           $xpathsources['xsd:' . $path]['description'] = $firstlang;
         }
-      }else{
+      }
+      else {
         $xpathsources['xsd:' . $path]['description'] = '';
       }
     }
