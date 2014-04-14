7fa90804c4f68c9fbfc695b973d7bb60b8e1d29f
Clemens Tolboom <clemens@build2be.com>
2014-04-14 14:44:24 +0200

    Adds some info for empty URI result by dumping . See also #50.

--- tests/manual/testXsdToObject.php
+++ tests/manual/testXsdToObject.php
@@ -4,10 +4,19 @@
 include(__DIR__ . '/../../src/XsdToObject.php');
 
 if (!isset($argv[1])) {
-  echo 'Usage: testXsdToObject [URI to XSD file]';
+  echo 'Usage: testXsdToObject [URI to XSD file]' . PHP_EOL;
   exit(1);
 }
 
 $test = new XsdToObject();
-$schema = $test->parse(file_get_contents($argv[1]));
-echo json_encode($schema, JSON_PRETTY_PRINT);
+
+$uri = $argv[1];
+$content = @file_get_contents($uri);
+if ($content) {
+  $schema = $test->parse($content);
+  echo json_encode($schema, JSON_PRETTY_PRINT);
+}
+else {
+  echo "No content found for $uri" . PHP_EOL;
+  var_dump($http_response_header);
+}
