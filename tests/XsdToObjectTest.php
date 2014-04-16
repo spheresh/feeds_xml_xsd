b7e43e63f33927e58225060319c7deafec3f7931
Clemens Tolboom <clemens@build2be.com>
2014-04-16 11:06:42 +0200

    Added more tests including data providers.

--- tests/XsdToObjectTest.php
+++ tests/XsdToObjectTest.php
@@ -6,6 +6,7 @@ class XsdToObjectTest extends PHPUnit_Framework_TestCase {
 
   function setup() {
     $this->xsd = new XsdToObject();
+    $this->setupGeoNovum();
   }
 
   function testXSD() {
@@ -69,30 +70,57 @@ EOF;
 
   }
 
+  function setupGeoNovum() {
+    $uri = __DIR__ . "/fixtures/STRI2012.xsd";
+    $contents = file_get_contents($uri);
+    $this->assertNotEmpty($contents, "Found XSD content");
+    $this->geonovum = $this->xsd->parse($contents);
+  }
+
   /**
    * This is our test reference file.
    *
    * http://schemas.geonovum.nl/stri/2012/1.0/STRI2012.xsd
+   *
+   * @dataProvider values
    */
-  function testGeoNovum() {
-    $uri = __DIR__ . "/fixtures/STRI2012.xsd";
-    $contents = file_get_contents($uri);
-    $this->assertNotEmpty($contents, "Found XSD content");
-
-    $result = $this->xsd->parse($contents);
-    var_dump($result);
-
-    $paths = array(
-      '/Manifest',
-      '/Manifest/Dossier',
-      '/Manifest/Dossier/Naam',
-      '/Manifest',
-
-    );
-
-    foreach ($paths as $path) {
-      $this->assertArrayHasKey($path, $result, $path . ' found');
-    }
+  function testGeoNovumValues($path) {
+    $this->assertArrayHasKey($path, $this->geonovum, $path . ' found');
   }
+
+  /**
+   * @dataProvider attributes
+   */
+  function testGeoNovumAttributes($path) {
+    $this->assertArrayHasKey($path, $this->geonovum, $path . ' found');
+  }
+
+  /**
+   * @dataProvider context
+   */
+  function testGeoNovumContext($path) {
+    $this->assertArrayHasKey($path, $this->geonovum, $path . ' found');
+  }
+
+  public function values() {
+    return $paths = array(
+      array('/Manifest/Dossier/Naam'),
+    );
+  }
+
+  public function attributes() {
+    return array(
+      array('/Manifest/@OverheidsCode'),
+      array('/Manifest/Dossier/@Id'),
+      array('/Manifest/Dossier/@Status'),
+    );
+  }
+
+  public function context() {
+    return array(
+      array('/Manifest'),
+      array('/Manifest/Dossier'),
+    );
+  }
+
 }
- 
