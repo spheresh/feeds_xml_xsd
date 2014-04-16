63d4c0182b8ff5a65f51f3db179f3efe047f3156
Clemens Tolboom <clemens@build2be.com>
2014-04-16 11:10:30 +0200

    No need to test for valid XSD so dropped code.

--- tests/XsdToObjectTest.php
+++ tests/XsdToObjectTest.php
@@ -9,67 +9,6 @@ class XsdToObjectTest extends PHPUnit_Framework_TestCase {
     $this->setupGeoNovum();
   }
 
-  function testXSD() {
-    // Not an XSD
-    $value = '<?xml version="1.0" encoding="UTF-8"?><x/>';
-    $result = $this->xsd->parse($value);
-    $this->assertEmpty($result, "Not a valid XSD");
-
-    // Smallest possible XSD
-    // http://www.vijaymukhi.com/documents/books/xsd/chap9.htm
-    $value = <<<EOF
-<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
-</xs:schema>
-EOF;
-    $result = $this->xsd->parse($value);
-    $this->assertEmpty($result, "Smallest possible XSD");
-
-    // Having an empty simpleType
-    $value = <<<EOF
-<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
-  <xs:simpleType name="Name">
-  </xs:simpleType>
-</xs:schema>
-EOF;
-
-    $result = $this->xsd->parse($value);
-    $this->assertEmpty($result, "Smallest possible XSD");
-
-    // element with simpleType Name
-    $value = <<<EOF
-<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
-  <xs:simpleType name="Name">
-    <xs:restriction base="xs:string"/>
-  </xs:simpleType>
-  <xs:element name="Name" type="Name"/>
-</xs:schema>
-EOF;
-
-    $result = $this->xsd->parse($value);
-    $this->assertNotEmpty($result, "element with simpleType Name XSD");
-
-
-    // Complex type Person with First and Last name.
-    $value = <<<EOF
-<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
-  <xs:simpleType name="First">
-    <xs:restriction base="xs:string"/>
-  </xs:simpleType>
-  <xs:simpleType name="Last">
-    <xs:restriction base="xs:string"/>
-  </xs:simpleType>
-  <xs:element name="Name" type="Name"/>
-</xs:schema>
-EOF;
-
-//    //$this->markTestSkipped("Skipping missing types. We do not test for valid XSD.");
-//    $result = $this->xsd->parse($value);
-//    var_dump($result);
-//    $this->fail("This should fail as there is no 'Name' type.");
-//    $this->assertNotEmpty($result, "element with simpleType Name XSD");
-
-  }
-
   function setupGeoNovum() {
     $uri = __DIR__ . "/fixtures/STRI2012.xsd";
     $contents = file_get_contents($uri);
