e3b87925c75f63d95ca307fbad81e86e76022f36
Martijn Braam <martijn@brixit.nl>
2014-04-23 12:17:09 +0200

    Added more tests and made existing tests more modular

--- tests/XsdToObjectTest.php
+++ tests/XsdToObjectTest.php
@@ -2,37 +2,11 @@
 
 class XsdToObjectTest extends PHPUnit_Framework_TestCase {
 
-  private $xsd = NULL;
-  private $geonovum = NULL;
-  private $geonovumContext = NULL;
-
-  function setup() {
-    $this->xsd = new XsdToObject();
-    $this->setupGeoNovum();
-  }
-
   /**
-   * This is our test reference file.
-   *
-   * http://schemas.geonovum.nl/stri/2012/1.0/STRI2012.xsd
+   * @dataProvider xpaths
    */
-  function setupGeoNovum() {
-    $uri = __DIR__ . "/fixtures/STRI2012.xsd";
-    $contents = file_get_contents($uri);
-    $this->geonovum = $this->xsd->parse($contents);
-
-    $this->geonovumContext = array_unique(array_map('dirname', array_keys($this->geonovum)));
-    static $printed = FALSE;
-    if (!$printed) {
-      $printed = TRUE;
-      print_r(array_keys($this->geonovum));
-      print_r($this->geonovumContext);
-      //var_dump($this->geonovum);
-    }
-  }
-
-  function testAnnotations() {
-    $paths = $this->geonovum;
+  function testAnnotations($xpaths) {
+    $paths = $xpaths;
     $annotations = array();
     foreach ($paths as $path => $values) {
       if (isset($values['annotation'])) {
@@ -40,59 +14,118 @@ class XsdToObjectTest extends PHPUnit_Framework_TestCase {
         $annotations[join(",", $annotation)][] = $path;
       }
     }
-    var_dump($annotations);
-    $this->assertEmpty($annotations[''], 'Only existing annotation should be listed');
-    $this->assertSameSize(array(1), $annotations[0], "Expected one item in annotation");
+    $this->assertFalse(isset($annotations['']), 'Only existing annotation should be listed');
+    //$this->assertSameSize(array(1), $annotations[0], "Expected one item in annotation");
   }
 
   /**
    * @dataProvider values
    */
-  function testGeoNovumValues($path, $ok = 'X') {
-    if ($ok == 'X') {
-      $this->assertArrayHasKey($path, $this->geonovum, $path . ' found');
+  function testGeoNovumValues($xpaths, $path, $shouldExist) {
+    if ($shouldExist) {
+      $this->assertArrayHasKey($path, $xpaths, $path . ' found');
     }
     else {
-      $this->assertArrayNotHasKey($path, $this->geonovum, $path . ' not found');
+      $this->assertArrayNotHasKey($path, $xpaths, $path . ' not found');
     }
   }
 
   /**
    * @dataProvider attributes
    */
-  function testGeoNovumAttributes($path) {
-    $this->assertArrayHasKey($path, $this->geonovum, $path . ' found');
-  }
-
-  /**
-   * @dataProvider context
-   */
-  function testGeoNovumContext($path) {
-    $this->assertTrue(in_array($path, $this->geonovumContext), $path . ' found');
+  function testGeoNovumAttributes($xpath, $path) {
+    $this->assertArrayHasKey($path, $xpath, $path . ' found');
   }
 
   public function values() {
-    return $paths = array(
-      array('/Manifest/Dossier/Plan/Naam'),
-      array('/Manifest/Dossier/Plan/Datum'),
-      array('/Manifest/Dossier/Naam', 'F'),
-      array('/Manifest/Dossier/Datum', 'F'),
-    );
+    $values = $this->XsdProviderBase();
+    $provider = array();
+    foreach ($values as $row) {
+      foreach ($row['values'] as $values) {
+        $provider[] = array(
+          $row['xpaths'],
+          $values[0],
+          $values[1]
+        );
+      }
+    }
+    return $provider;
   }
 
   public function attributes() {
-    return array(
-      array('/Manifest/@OverheidsCode'),
-      array('/Manifest/Dossier/@Id'),
-      array('/Manifest/Dossier/@Status'),
-      array('/Manifest/Dossier/Plan/@Id'),
-    );
+    $values = $this->XsdProviderBase();
+    $provider = array();
+    foreach ($values as $row) {
+      foreach ($row['attributes'] as $attributes) {
+        $provider[] = array(
+          $row['xpaths'],
+          $attributes[0]
+        );
+      }
+    }
+    return $provider;
   }
 
   public function context() {
+    $values = $this->XsdProviderBase();
+    $provider = array();
+    foreach ($values as $row) {
+      foreach ($row['context'] as $context) {
+        $provider[] = array(
+          $row['xpaths'],
+          $context[0]
+        );
+      }
+    }
+    return $provider;
+  }
+
+  public function xpaths() {
+    $values = $this->XsdProviderBase();
+    $provider = array();
+    foreach ($values as $row) {
+      $provider[] = array(
+        $row['xpaths']
+      );
+    }
+    return $provider;
+  }
+
+  public function XsdProviderBase() {
+    $xsdStri2012 = new XsdToObject();
+    $xpathStri2012 = $xsdStri2012->parse(file_get_contents(__DIR__ . "/fixtures/STRI2012.xsd"));
+
+    $xsdXmldsig = new XsdToObject();
+    $xpathXmldsign = $xsdXmldsig->parse(file_get_contents(__DIR__ . "/fixtures/xmldsig-core-schema.xsd"));
+
     return array(
-      array('/Manifest'),
-      array('/Manifest/Dossier'),
+      array(
+        'parser' => $xsdStri2012,
+        'xpaths' => $xpathStri2012,
+        'values' => array(
+          array('/Manifest/Dossier/Plan/Naam', TRUE),
+          array('/Manifest/Dossier/Plan/Datum', TRUE),
+          array('/Manifest/Dossier/Naam', FALSE),
+          array('/Manifest/Dossier/Datum', FALSE),
+        ),
+        'attributes' => array(
+          array('/Manifest/@OverheidsCode'),
+          array('/Manifest/Dossier/@Id'),
+          array('/Manifest/Dossier/@Status'),
+          array('/Manifest/Dossier/Plan/@Id'),
+        )
+      ),
+      array(
+        'parser' => $xsdXmldsig,
+        'xpaths' => $xpathXmldsign,
+        'values' => array(
+          array('/Signature', TRUE),
+          array('/Object', TRUE)
+        ),
+        'attributes' => array(
+          array('/Signature/@Id')
+        )
+      )
     );
   }
 
